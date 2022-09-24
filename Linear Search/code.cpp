a9da0afd329484d7d047d298e7d616a063480444
Hardik Singh <91072109+Hardik-Singh04@users.noreply.github.com>
2022-09-24 21:00:37 +0530

    Update code.cpp

--- Linear Search/code.cpp
+++ Linear Search/code.cpp
@@ -1,2 +1,25 @@
 /* Program to Implement Linear Search Method and also find
 Total No of Comparisons.*/
+#include<iostream> 
+using namespace std; 
+bool linearSearch(int arr[] , int n , int key) 
+{ 
+for(int i = 0 ; i < n ; i++)
+{ 
+if(arr[i] == key) 
+return true; 
+} 
+return false; 
+} 
+int main()
+{ 
+int n , key; 
+cin>>n; 
+int arr[n]; 
+for(int i=0 ; i < n ; i++) 
+cin>>arr[i]; 
+cout<<"Enter an key "; 
+cin>>key; 
+linearSearch(arr , n , key) == true ?cout<<"key is Present" : cout<<"Key is Not Present"; 
+return 0; 
+}
