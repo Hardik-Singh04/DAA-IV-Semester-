1b0dff657e0dc40d8f964816b595810b18137c7b
Hardik Singh <91072109+Hardik-Singh04@users.noreply.github.com>
2022-09-24 21:03:09 +0530

    Create code.cpp

---
+++ Searching Element/code.cpp
@@ -0,0 +1,4 @@
+/* Given an already sorted array of positive integers, 
+design an algorithm and implement it using a program to find whether a given 
+key element is present in the array or not. Also, find the total number of 
+comparisons for each input case.*/
