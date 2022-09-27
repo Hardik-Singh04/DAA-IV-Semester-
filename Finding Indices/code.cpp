2172c030ebb991b9f8569055f9d88b9e4eb6d937
Hardik Singh <91072109+Hardik-Singh04@users.noreply.github.com>
2022-09-27 17:54:53 +0530

    Update code.cpp

--- Finding Indices/code.cpp
+++ Finding Indices/code.cpp
@@ -2,3 +2,43 @@
 algorithm and implement it using a program to find three indices i, j, k such 
 that arr[i] + arr[j] = arr[k]. Your Program Should run for T no of Test cases 
 inputed by user.*/
+#include<bits/stdc++.h> 
+using namespace std; 
+void sum(int arr[],int n) 
+{ 
+bool flag=true; 
+for(int k=0;k<n;k++) 
+{ int i=0; int 
+j=n-1; 
+while(i<j)
+{
+if(arr[i]+arr[j] == arr[k])
+{ 
+if(flag) 
+flag=false; 
+cout<<"Sequence: "<<i<<" "<<j<<" "<<k<<endl; 
+i++; 
+j--; 
+} 
+if(arr[i]+arr[j] <arr[k]) 
+i++; 
+else 
+j--; 
+} 
+} 
+if(flag) 
+{ 
+cout<<"No Sequence found"<<endl; 
+} 
+} 
+int main() 
+{ int n; 
+cin>>n; 
+int arr[n]; 
+for(int i=0;i<n;i++) 
+{ 
+cin>>arr[i]; 
+} 
+sum(arr,n); 
+return 0; 
+} 
